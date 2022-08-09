5aa1c7fe905a4a2c3ec89096d62eb4e777338af7
Arthur Leywin <110176533+kamatashi@users.noreply.github.com>
2022-08-09 13:39:09 -0300

    Produtor Escalar

--- app/src/calculator/vga.js
+++ app/src/calculator/vga.js
@@ -4,13 +4,19 @@ const trigonometria = require("./trigonometria")
 const vetorValue = vetor => vetor[1] - vetor[0]
 const moduloVetor = vetor => Math.sqrt( (vetor[0]**2) + (vetor[1]**2) )
 
-
 // Soma e subtração de vetores oblíquos
 const modulo_VetorResultante = vetor => coseno => moduloVetor(vetor) + 2 * vetor[0] * vetor[1] * coseno
 
-const teste = trigonometria.soma_arcos.sen_soma(10)
-console.log(teste)
+
+// Multiplicador, usado quando queremos multiplicar o vetor
+const multVetorHelper = (multiplicador) => (vetor) => vetor*multiplicador
+const mult_selected = 5
+function multVetor_selected (vetor) {
+    return multVetorHelper(mult_selected)(vetor)
+}
 
 
-const vetor = [3, 2]
-console.log(moduloVetor(vetor))
+
+// PRODUTO ESCALAR
+const produtoEscalar_Vetores = (vetor1, vetor2) => vetor1[0]*vetor2[0] + vetor1[1]*vetor2[1]
+//const angulo_entreVetores = 
